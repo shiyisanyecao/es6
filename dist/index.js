c79fa5d22e7481c91c3d675de2bb1ab306b3c6aa
shiyisanyecao <2327532812@qq.com>
2018-12-26 19:44:47 +0800

    增加了map

--- dist/index.js
+++ dist/index.js
@@ -1,58 +1,21 @@
 'use strict';
 
-// Set 数据结构，去重
-var setArr = new Set(['jspang', '技术胖', 'web']);
-
-setArr.add('前端职场');
-
-setArr.delete('web');
-console.log(setArr);
-
-var _iteratorNormalCompletion = true;
-var _didIteratorError = false;
-var _iteratorError = undefined;
-
-try {
-  for (var _iterator = setArr[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-    var item = _step.value;
-
-    console.log(item);
-  }
-} catch (err) {
-  _didIteratorError = true;
-  _iteratorError = err;
-} finally {
-  try {
-    if (!_iteratorNormalCompletion && _iterator.return) {
-      _iterator.return();
-    }
-  } finally {
-    if (_didIteratorError) {
-      throw _iteratorError;
-    }
-  }
-}
-
-console.log(setArr.size);
-setArr.forEach(function (item) {
-  console.log(item);
-});
-
-// 查找has
-// console.log(setArr.has('jspang'));
-console.log(setArr);
-
-// setArr.clear();
-// console.log(setArr);
-
-
-var weakObj = new WeakSet();
-var obj = {
-  a: 'jspang',
-  b: 'web'
+var json = {
+  name: 'jspang',
+  skill: 'web'
 };
-var obj1 = obj;
-weakObj.add(obj);
-weakObj.add(obj1);
+console.log(json.name);
 
-console.log(weakObj);
+var map = new Map();
+map.set(json, 'iam');
+console.log(map);
+map.set('jspang', json);
+console.log(map);
+map.set('skills', 'webb');
+console.log(map);
+
+console.log(map.get(json));
+map.delete('skills');
+console.log(map);
+
+console.log(map.has('jspang'));
---
+++ src/map.js
@@ -0,0 +1,19 @@
+let json = {
+  name: 'jspang',
+  skill: 'web'
+}
+console.log(json.name);
+
+var map = new Map();
+map.set(json,'iam');
+console.log(map);
+map.set('jspang',json);
+console.log(map);
+map.set('skills','webb');
+console.log(map);
+
+console.log(map.get(json));
+map.delete('skills');
+console.log(map);
+
+console.log(map.has('jspang'));
